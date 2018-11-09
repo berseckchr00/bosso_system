d6c357d0e52bde790f8b976d1f41d03c5a6b949e
Diego Gonzalez <diego.gonzalez@grupogtd.com>
2018-11-09 17:40:00 -0300

    template fix default

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -17,7 +17,7 @@
         <link rel="stylesheet" href="{{asset('css/AdminLTE.min.css')}}">
         <!-- AdminLTE Skins. Choose a skin from the css/skins
             folder instead of downloading all of them to reduce the load. -->
-        <link rel="stylesheet" href="{{asset('css/skins/skin-black.min.css')}}">
+        <link rel="stylesheet" href="{{asset('css/skins/_all-skins.min.css')}}">
         <!-- Google Font -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
     </head>
